80a665f242fc0f8dbf2f5844080ee3d8f948f400
NicolasVannelli <Nicolas_Vannelli@hotmail.com>
2023-11-24 01:29:41 -0300

    Agregé la funcionalidad de eliminar pacientes

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Que tengo que hacer
-//Agregar Tareas
-//Mostrar Tareas
-//Eliminar Tareas
-//Mostrar tareas como completadas
+//Agregar Pacientes  *
+//Mostrar Pacientes  *
+//Eliminar Pacientes *
+//Agregar un cambio de datos de pacientes por algun error con el metodo splice
+
 
 
 const Paciente = function (nombre, edad, sangre) {
@@ -17,7 +18,8 @@ let pacientes = []
 function inicilizar() {
     let ejecutar = confirm("¿Quiere utilizar esta aplicación?")
     while (ejecutar) {
-        let options = prompt("A para agregar paciente,B para buscar paciente,Q para quitar paciente y S para salir").toUpperCase()
+        let options = prompt("A para agregar paciente, B para buscar paciente, Q para quitar paciente, M para mostrar pacientes y S para salir").toUpperCase()
+        //Esto seria el menú de opciones, a medida que se agregen mas puedo poner otro case 
         switch (options) {
             case "A":
                 AgregarPaciente()
@@ -34,11 +36,22 @@ function inicilizar() {
                 }
                 break
             case "Q":
-                alert("Funcion en desarrollo")
+                EliminarPaciente()
+                ejecutar=confirm("¿Desea seguir utilizando la aplicación?")
+                if(ejecutar===false){
+                    alert("Saliendo, gracias por utilizar nuestra aplicación")
+                }
                 break
             case "S":
                 alert("Saliendo, gracias por utilizar nuestra aplicación")
                 ejecutar = false
+                break
+            case "M":
+                console.table(pacientes)
+                ejecutar=confirm("¿Desea seguir utilizando la aplicación?")
+                if(ejecutar===false){
+                    alert("Saliendo, gracias por utilizar nuestra aplicación")
+                }
                 break
             default:
                 alert("Opción invalida")
@@ -47,9 +60,9 @@ function inicilizar() {
     }
 }
 function AgregarPaciente() {
-    let nombre = prompt("Ingrese el nombre de la persona")
+    let nombre = prompt("Ingrese el nombre de la persona").toUpperCase()
     let edad = prompt("¿Cuál es su edad?")
-    let sangre = prompt("¿Cuál es el tipo de sangre?")
+    let sangre = prompt("¿Cuál es el tipo de sangre?").toUpperCase()
     let add = new Paciente(nombre, edad, sangre)
     pacientes.push(add)
     alert("¡Los datos han sido ingresados con éxito!")
@@ -75,4 +88,21 @@ function Filtrar(){
     }
 
 }
+function EliminarPaciente(){
+    let buscar = prompt("¿A quién quiere eliminar?").toUpperCase()
+    let resultado= pacientes.filter((x) => x.nombre.toUpperCase().includes(buscar))
+    if (resultado.length > 0) {
+                    //Esto es para obtener el primer indice que coincida con la busqueda
+        let pacienteAEliminar = pacientes.indexOf(resultado[0])
+
+        if (pacienteAEliminar !== -1) {
+            //Aca utilizo el splice para eliminar el indice anteriormente obtenido,el 1 significa la cantidad de elementos a eliminar
+            pacientes.splice(pacienteAEliminar, 1)
+            console.table(pacientes)
+        } else {
+            console.log("Paciente no encontrado en la lista.")
+        }
+    }
+}
+
 inicilizar()
